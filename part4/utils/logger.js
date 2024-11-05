3d9665371373a525c2a411c73578589d453544a3
ChenFangFangFang <sophiachan0430@gmail.com>
2024-11-05 11:20:59 +0200

    add some blog tests

--- part4/utils/logger.js
+++ part4/utils/logger.js
@@ -1,10 +1,14 @@
 const info = (...params) => {
-    console.log(...params)
+    if (process.env.NODE_ENV !== 'test') {
+        console.log(...params)
+    }
 }
 //info for printing normal log messages
 //error for all error messages
 const error = (...params) => {
-    console.error(...params)
+    if (process.env.NODE_ENV !== 'test') {
+        console.error(...params)
+    }
 }
 
 module.exports = {
