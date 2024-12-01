7db4643d3628d13e0614fa4394a9628821c7cf27
ChenFangFangFang <sophiachan0430@gmail.com>
2024-12-01 16:12:51 +0200

    show user's blog amount

--- blog_app/bloglist-frontend/src/components/Users.jsx
+++ blog_app/bloglist-frontend/src/components/Users.jsx
@@ -1,22 +1,40 @@
-import users from "../services/users";
 import { useSelector, useDispatch } from "react-redux";
 import { initializeUsers } from "../reducers/allUsersReducer";
 import { useEffect } from "react";
+
+const ShowUsersBlog = ({ users }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>User</th>
+          <th>BLogs Created</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user) => (
+          <tr key={user.id}>
+            <td>{user.name}</td>
+            <td>{user.blogs.length}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 const Users = () => {
   const dispatch = useDispatch();
   const users = useSelector((state) => state.users);
+
   useEffect(() => {
     dispatch(initializeUsers());
   }, [dispatch]);
   console.log("All users: ", users);
+
   return (
     <div>
       <h2>Users</h2>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
+      <ShowUsersBlog users={users} />
     </div>
   );
 };
